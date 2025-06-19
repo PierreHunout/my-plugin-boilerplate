cde4c4c46c0f8b2438c68ac3de0328d1a737c775
PierreHunout <pierre.hunout@gmail.com>
2025-06-19 19:25:59 +0100

    Add Comments to the plugin

--- includes/Front/Page.php
+++ includes/Front/Page.php
@@ -1,18 +1,37 @@
 <?php
-
 /**
-* @package WpPluginBoilerplate
-*/
+ * This file is responsible for handling the page functionality in the WordPress plugin.
+ * It includes methods to run the page functionality and can be extended in the future.
+ * 
+ * @package WpPluginBoilerplate
+ * @since 1.0.0
+ * @version 1.0.0
+ * @author Pierre Hunout <https://pierrehunout.com/>
+ */
 
 namespace WpPluginBoilerplate\Front;
+
+/**
+ * This check prevents direct access to the plugin file,
+ * ensuring that it can only be accessed through WordPress.
+ * 
+ * @since 1.0.0
+ */
+if (!defined('WPINC')) {
+    die;
+}
 
 class Page
 {
     /**
-     * Class Runner.
+     * Class Runner for the page functionality.
      * 
+     * This function is currently empty and can be extended in the future
+     * to include page-related features or hooks.
+     * 
+     * @return void
      */
-    public static function run()
+    public function run()
     {
 
     }
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Code so quiet, you can hear the silence.
+ * 
+ * This file is intentionally left blank to prevent direct access.
+ * It serves as a placeholder to ensure that the plugin's main file is not directly accessed.
+ */
